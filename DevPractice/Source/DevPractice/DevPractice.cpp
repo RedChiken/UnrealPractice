0b70adb78f8215d6f3dfc5a4de0748e7278b7f1f
RedChiken <gombabo1217@gmail.com>
2022-07-08 19:40:58 +0900

    Initialize Project

---
+++ DevPractice/Source/DevPractice/DevPractice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DevPractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DevPractice, "DevPractice" );
---
+++ DevPractice/Source/DevPractice/DevPracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DevPracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVPRACTICE_API ADevPracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
