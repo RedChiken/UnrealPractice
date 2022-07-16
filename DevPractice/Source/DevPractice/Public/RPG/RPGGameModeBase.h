50a8704f79d6997b47b8b603f9e5a64dbb6409d9
RedChiken <gombabo1217@gmail.com>
2022-07-17 05:11:16 +0900

    [RPG] GameMode, Character 관련 부차 Class/BP 생성, GameMode에 연결

---
+++ DevPractice/Source/DevPractice/Public/RPG/RPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DevPractice/DevPracticeGameModeBase.h"
+#include "RPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVPRACTICE_API ARPGGameModeBase : public ADevPracticeGameModeBase
+{
+	GENERATED_BODY()
+	
+};
